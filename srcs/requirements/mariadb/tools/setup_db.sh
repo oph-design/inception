161df0f9b64852d3e7b12d006eed8ae2dc789d90
oph-design <ole.paul.heinzelmann@gmail.com>
2023-07-18 20:17:55 +0200

    implement somewhat working db server

--- srcs/requirements/mariadb/tools/setup_db.sh
+++ srcs/requirements/mariadb/tools/setup_db.sh
@@ -1,2 +1,11 @@
+#!/bin/sh
 /etc/init.d/mariadb setup
+echo $?
 /etc/init.d/mariadb start
+echo $?
+mysql -u root < /tmp/create_db.sql
+echo $?
+rc-service mariadb stop
+echo $?
+/usr/bin/mysqld --user mysql
+echo $?
