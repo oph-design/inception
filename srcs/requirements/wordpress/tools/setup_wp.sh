934431e6d0eb02a62feb25850d3d383afcc6f6d0
oph-design <ole.paul.heinzelmann@gmail.com>
2023-07-20 18:54:59 +0200

    implement docker-compose

--- srcs/requirements/wordpress/tools/setup_wp.sh
+++ srcs/requirements/wordpress/tools/setup_wp.sh
@@ -1,4 +1,3 @@
-wp-cli config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS --locale=en_DB
 wp-cli core install --url=$DOMAIN --admin_user=$WP_USER --admin_password=$WP_PASS --admin_email=$WP_EMAIL
 php-fpm8 --nodaemonize
 
