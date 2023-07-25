9e706cfc0043b4b6cb32c24f86ea0fa922138d6b
oph-design <ole.paul.heinzelmann@gmail.com>
2023-07-25 21:20:07 +0200

    change ngnix config for php-fpm use'

--- srcs/requirements/wordpress/tools/setup_wp.sh
+++ srcs/requirements/wordpress/tools/setup_wp.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sleep 10
-wp-cli core install --title=raffaelhoe --url=$WP_HOST --admin_user=$WP_USER --admin_password=$WP_PASS --admin_email=$WP_MAIL
-php-fpm81 --nodaemonize
+wp-cli core install --title=inception --url=$WP_HOST --admin_user=$WP_USER --admin_password=$WP_PASS --admin_email=$WP_MAIL
+php-fpm81 -F
