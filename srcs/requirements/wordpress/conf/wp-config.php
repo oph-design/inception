f4073952c6011340ca3c19d30bdc90bbeb2b7d23
oph-design <ole.paul.heinzelmann@gmail.com>
2023-07-26 15:32:31 +0200

    setup working wordpress

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -37,6 +37,7 @@ define( 'DB_CHARSET', 'utf8' );
 /** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
 
+define( 'WP_HOME', getenv('WP_HOST') );
 /**#@+
  * Authentication unique keys and salts.
  *
--- srcs/requirements/wordpress/tools/setup_wp.sh
+++ srcs/requirements/wordpress/tools/setup_wp.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-sleep 10
-wp-cli core install --title=inception --url=$WP_HOST --admin_user=$WP_USER --admin_password=$WP_PASS --admin_email=$WP_MAIL
+sleep 30
+wp-cli core install --title=inception --url=$WP_HOST --admin_user=$WP_ADMN --admin_password=$WP_ADMP --admin_email=$WP_MAIL
 php-fpm81 -F
