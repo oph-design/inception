c1b19d28cf7c37b9a2b0d887a26b1b90d83cf77b
oph-design <ole.paul.heinzelmann@gmail.com>
2023-07-26 16:08:04 +0200

    change setup_wp scipt

--- srcs/requirements/wordpress/tools/setup_wp.sh
+++ srcs/requirements/wordpress/tools/setup_wp.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
-sleep 30
-wp-cli core install --title=inception --url=$WP_HOST --admin_user=$WP_ADMN --admin_password=$WP_ADMP --admin_email=$WP_MAIL
+while ! wp-cli core install --title=inception \
+        --url=$WP_HOST --admin_user=$WP_ADMN \
+        --admin_password=$WP_ADMP --admin_email=$WP_MAIL
+do
+  sleep 1
+done
+wp-cli user create $WP_USER $WP_MAIL --user_pass=$WP_PASS
 php-fpm81 -F
