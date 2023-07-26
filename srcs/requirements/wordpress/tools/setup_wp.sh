f9871ffbbc2d6d7c17b150adeae4991058a9e408
oph-design <ole.paul.heinzelmann@gmail.com>
2023-07-26 20:25:22 +0200

    change index to wp-login.php

--- srcs/requirements/wordpress/tools/setup_wp.sh
+++ srcs/requirements/wordpress/tools/setup_wp.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-while ! wp-cli core install --title=inception \
+while ! wp-cli core install --title=$WP_TITLE \
         --url=$WP_HOST --admin_user=$WP_ADMIN_USER \
         --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_MAIL
 do
